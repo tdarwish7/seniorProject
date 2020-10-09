deb5056d9d428a18d37cb12f35d9f56d6fe61572
tdarwish <darwishtariq21@gmail.com>
2020-10-09 13:35:27 -0400

    created base project with homepage

---
+++ src/main/java/com/educationwebapplication/educationwebapplication/EducationWebApplication.java
@@ -0,0 +1,13 @@
+package com.educationwebapplication.educationwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educationwebapplication/educationwebapplication/EducationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationwebapplication.educationwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
