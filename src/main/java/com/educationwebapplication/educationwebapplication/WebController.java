ad483d368b1d30253146c54b3c17eea4dec923ae
tdarwish <darwishtariq21@gmail.com>
2020-10-12 10:15:03 -0400

    added a basic web controller

---
+++ src/main/java/com/educationwebapplication/educationwebapplication/WebController.java
@@ -0,0 +1,22 @@
+package com.educationwebapplication.educationwebapplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+/*
+* This Java class is in charge of routing our pages to different urls'.
+* When adding or creating a new web page. Add the code block below.
+* Change the name of the url you'd like to use to route to, to the
+* proper name of the web page and add a new HTML5 file in the templates
+* folder.
+*
+*
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }*/
+}
