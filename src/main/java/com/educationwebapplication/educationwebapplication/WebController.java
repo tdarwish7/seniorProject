5936b789b34b9e55b312f8ff05eb54b5f158c259
tdarwish <darwishtariq21@gmail.com>
2020-10-15 13:28:31 -0400

    add CDN's for mdbootstrap and created a basic navigation menu that is mobilly responsive.

--- src/main/java/com/educationwebapplication/educationwebapplication/WebController.java
+++ src/main/java/com/educationwebapplication/educationwebapplication/WebController.java
@@ -7,16 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-/*
-* This Java class is in charge of routing our pages to different urls'.
-* When adding or creating a new web page. Add the code block below.
-* Change the name of the url you'd like to use to route to, to the
-* proper name of the web page and add a new HTML5 file in the templates
-* folder.
-*
-*
-    @RequestMapping("/home")
-    public String home() {
-        return "home";
-    }*/
+    @RequestMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
